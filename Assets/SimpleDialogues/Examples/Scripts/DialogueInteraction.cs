8e06e770a755ab16185ee8dfdb0a4c95797c1772
NickIs211 <mrflyingbananas@yahoo.com>
2018-10-13 04:14:34 -0500

    Working on dialogue controller

--- Assets/SimpleDialogues/Examples/Scripts/DialogueInteraction.cs
+++ Assets/SimpleDialogues/Examples/Scripts/DialogueInteraction.cs
@@ -2,40 +2,49 @@
 using UnityEngine.UI;
 
 public class DialogueInteraction : MonoBehaviour {
-
-    [SerializeField]
-    Dialogues npc;
-
+	private Dialogues dialogues;
     [SerializeField]
     Text dialogueText;
     [SerializeField]
-    Text leftText;
+    Text topText;
+	[SerializeField]
+	Text middleText;
+	[SerializeField]
+    Text bottomText;
     [SerializeField]
-    Text rightText;
-    [SerializeField]
-    Text middleText;
-    [SerializeField]
-    GameObject backPanel;
-    [SerializeField]
-    GameObject nextTreeButton;
+    GameObject dialogueUI;
+	[SerializeField]
+	GameObject buttonContainer;
+	[SerializeField]
+	GameObject dialogueContainer;
+	[SerializeField]
+	GameObject portrait;
 
-    bool nextEnd = false;
-
-    // Simple Dialogues //
-    // This is a basic example of how you can use the dialogue system. //
+	bool nextEnd = false;
+	
 
     
 	void Start() {
-        npc.SetTree("FirstMeeting"); //This sets the current tree to be used. Resets to the first node when called.
-        Display();
+		Hide();
+	}
+
+	public void Hide()
+	{
+		dialogueUI.SetActive(false);
+	}
+
+	public void StartConversation(GameObject NPC)
+	{
+		portrait.GetComponent<Image>().sprite = NPC.GetComponent<NPCData>().portrait;
+		dialogues = NPC.GetComponent<Dialogues>();
 	}
 
     public void Choice(int index)
     {
-        if (index == 2 && npc.GetCurrentTree() == "TalkAgain") index = 1;
-        if (npc.GetChoices().Length != 0)
+        if (index == 2 && dialogues.GetCurrentTree() == "TalkAgain") index = 1;
+        if (dialogues.GetChoices().Length != 0)
         {
-            npc.NextChoice(npc.GetChoices()[index]); //We make a choice out of the available choices based on the passed index.
+            dialogues.NextChoice(dialogues.GetChoices()[index]); //We make a choice out of the available choices based on the passed index.
             Display();                               //We actually call this function on the left and right button's onclick functions
         }
         else
@@ -46,14 +55,14 @@ public class DialogueInteraction : MonoBehaviour {
 
     public void TalkAgain()
     {
-        npc.SetTree("TalkAgain");
+        dialogues.SetTree("TalkAgain");
         nextEnd = false;
         Display();
     }
 
     public void Progress()
     {
-        npc.Next(); //This function returns the number of choices it has, in my case I'm checking that in the Display() function though.
+        dialogues.Next(); //This function returns the number of choices it has, in my case I'm checking that in the Display() function though.
         Display();
     }
 
@@ -61,35 +70,34 @@ public class DialogueInteraction : MonoBehaviour {
     {
         if (nextEnd == true)
         {
-            backPanel.SetActive(false);
+            dialogueUI.SetActive(false);
             nextTreeButton.SetActive(true);
         }
         else
         {
-            backPanel.SetActive(true);
-            nextTreeButton.SetActive(false);
+            dialogueUI.SetActive(true);
         }
 
         //Sets our text to the current text
-        dialogueText.text = npc.GetCurrentDialogue();
+        dialogueText.text = dialogues.GetCurrentDialogue();
         //Just debug log our triggers for example purposes
-        if (npc.HasTrigger())
-            Debug.Log("Triggered: "+npc.GetTrigger());
+        if (dialogues.HasTrigger())
+            Debug.Log("Triggered: "+dialogues.GetTrigger());
         //This checks if there are any choices to be made
-        if (npc.GetChoices().Length != 0)
+        if (dialogues.GetChoices().Length != 0)
         {
             //Setting the text's of the buttons to the choices text, in my case I know I'll always have a max of three choices for this example.
-            leftText.text = npc.GetChoices()[0];
-            middleText.text = npc.GetChoices()[1];
+            topText.text = dialogues.GetChoices()[0];
+            middleText.text = dialogues.GetChoices()[1];
             //If we only have two choices, adjust accordingly
-            if (npc.GetChoices().Length > 2)
-                rightText.text = npc.GetChoices()[2];
+            if (dialogues.GetChoices().Length > 2)
+                bottomText.text = dialogues.GetChoices()[2];
             else
-                rightText.text = npc.GetChoices()[1];
+                bottomText.text = dialogues.GetChoices()[1];
             //Setting the appropriate buttons visability
-            leftText.transform.parent.gameObject.SetActive(true);
-            rightText.transform.parent.gameObject.SetActive(true);
-            if(npc.GetChoices().Length > 2)
+            topText.transform.parent.gameObject.SetActive(true);
+            bottomText.transform.parent.gameObject.SetActive(true);
+            if(dialogues.GetChoices().Length > 2)
                 middleText.transform.parent.gameObject.SetActive(true);
             else
                 middleText.transform.parent.gameObject.SetActive(false);
@@ -98,12 +106,12 @@ public class DialogueInteraction : MonoBehaviour {
         {
             middleText.text = "Continue";
             //Setting the appropriate buttons visability
-            leftText.transform.parent.gameObject.SetActive(false);
-            rightText.transform.parent.gameObject.SetActive(false);
+            topText.transform.parent.gameObject.SetActive(false);
+            bottomText.transform.parent.gameObject.SetActive(false);
             middleText.transform.parent.gameObject.SetActive(true);
         }
         
-        if (npc.End()) //If this is the last dialogue, set it so the next time we hit "Continue" it will hide the panel
+        if (dialogues.End()) //If this is the last dialogue, set it so the next time we hit "Continue" it will hide the panel
             nextEnd = true;
     }
 }
