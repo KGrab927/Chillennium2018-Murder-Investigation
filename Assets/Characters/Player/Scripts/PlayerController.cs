9937fc3065ce6a666817d9086bab9a9ecd781fff
NickIs211 <mrflyingbananas@yahoo.com>
2018-10-13 01:16:00 -0500

    Added player animation

--- Assets/Characters/Player/Scripts/PlayerController.cs
+++ Assets/Characters/Player/Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
 
 	public float speed;            
 	
-	private Rigidbody2D rb2d;     
-
+	private Rigidbody2D rb2d;
+	private Animator anim;
 	
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 		rb2d.freezeRotation = true;
 	}
 
@@ -26,11 +27,14 @@ public class PlayerController : MonoBehaviour
 		if(movement.magnitude == 0)
 		{
 			rb2d.velocity = new Vector2(Mathf.Lerp(rb2d.velocity.x, 0, .8f), Mathf.Lerp(rb2d.velocity.y, 0, .8f));
+			anim.SetBool("moving", false);
 		}
 		else
 		{
 			rb2d.velocity = (movement.normalized * speed);
+			anim.SetBool("moving", true);
+			anim.SetFloat("walk_dir_x", moveHorizontal);
+			anim.SetFloat("walk_dir_y", moveVertical);
 		}
-
 	}
 }
