1bb21f0154c6cffdf67eca853833698037ff609e
NickIs211 <mrflyingbananas@yahoo.com>
2018-10-12 23:38:28 -0500

    Player movement

---
+++ Assets/Camera/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float dist = Mathf.Abs(Screen.height / 2 - Camera.main.WorldToScreenPoint(player.transform.position).y);
+
+		if(dist > Screen.height/4)
+		{
+			transform.Translate(0, Mathf.Lerp(transform.position.y,player.transform.position.y, .6f), 0);
+		}
+	}
+}
---
+++ Assets/Characters/Player/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;            
+	
+	private Rigidbody2D rb2d;     
+
+	
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.freezeRotation = true;
+	}
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
+
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		
+		if(movement.magnitude == 0)
+		{
+			rb2d.velocity = new Vector2(Mathf.Lerp(rb2d.velocity.x, 0, .8f), Mathf.Lerp(rb2d.velocity.y, 0, .8f));
+		}
+		else
+		{
+			rb2d.velocity = (movement.normalized * speed);
+		}
+
+	}
+}
