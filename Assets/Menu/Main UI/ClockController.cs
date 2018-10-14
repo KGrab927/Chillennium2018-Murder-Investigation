4b207d62e5e68fe4f3efdd1df88135bcdc9f887c
NickIs211 <mrflyingbananas@yahoo.com>
2018-10-14 03:57:47 -0500

    Re-did dialogue menu

--- Assets/Menu/Main UI/ClockController.cs
+++ Assets/Menu/Main UI/ClockController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClockController : MonoBehaviour {
+	[SerializeField]
+	GameObject minuteHand;
+	[SerializeField]
+	GameObject hourHand;
+	[SerializeField]
+	float minutesToEnd;
+	[SerializeField]
+	Animator anim;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		minuteHand.transform.Rotate(new Vector3(0,0,-1), ((180 / minutesToEnd) / 60) * Time.deltaTime);
+		hourHand.transform.Rotate(new Vector3(0, 0, -1), ((15 / minutesToEnd) / 60) * Time.deltaTime);
+
+		if(hourHand.transform.rotation.z >= 90)
+		{
+			anim.SetTrigger("FadeOut");
+		}
+	}	
 }
