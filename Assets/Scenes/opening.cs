90df151447aa9eb1ea7c585025e7982e1e0bb966
Kyle Grabfelder <kylegrabfelder@hotmail.com>
2018-10-14 10:28:22 -0500

    Scene changes

---
+++ Assets/Scenes/opening.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opening : MonoBehaviour {
+
+	GameObject[] players;
+
+	int currDialogIndex;
+
+	string[] order;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+		currDialogIndex = 0;
+		order = new string[] {"Best Friend", "Priest", "Butler", "Dead Player"};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
